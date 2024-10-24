8a9fbe2f976e29f3635ee8b181365150defc62c7
Roy <roy154638@gmail.com>
2024-10-24 16:02:35 +0800

    Modify main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str # 必填
+    price: float
+    description: str =None # 非必填
 
 @app.get("/") # 路徑名稱
 def read_root(): # 呼叫此function
@@ -10,3 +15,8 @@ def read_root(): # 呼叫此function
 @app.get("/happy") # 路徑名稱
 def read_happy(): # 呼叫此function
     return "I am so happy." # 執行function程式
+
+@app.get("/items/") # 路徑名稱
+def creat_item(item: Item): # 呼叫此function
+    print(f"Recive item: {item}")
+    return {"message": "Item recived" ,"item": item} # 執行function程式
