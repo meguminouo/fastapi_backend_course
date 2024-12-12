e054768e7a39d7c2c9ad34b538b390391e087b5a
meguminouo <pajh103086@gmail.com>
2024-12-12 15:37:55 +0800

    Add user base

--- app/models.py
+++ app/models.py
@@ -9,3 +9,11 @@ class Todo(Base):
     descrption = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), nullable=False)
+    password = Column(String(255), nullable=False)
+    email = Column(String(100), nullable=False)
